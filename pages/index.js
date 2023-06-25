f38cc7e301c1f40865c3d5b7d52cc270dd6b9f37
Erika Sugai <erika.watershell@gmail.com>
2023-06-25 19:08:36 -0400

    Add Welcome Message

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          It's time to chill the fuck out :)
         </p>
       </main>
 
